d411c5b322bea08cf9070cc751f14780affa1acb
Lukaa255 <lukabrand254@gmail.com>
2026-03-29 12:36:05 -0700

    Update quran.js

--- lucvoice/quran.js
+++ lucvoice/quran.js
@@ -46,7 +46,7 @@ zokou({
     
     const data = response.data.data;
     const messageText = `
-ᬑ *NEXUS-AI QURAN SURAH* ᬒ
+ᬑ *LUCVOICE-XMD QURAN SURAH* ᬒ
 
 *🕌 Quran: The Holy Book*
 📜 *Surah:* 🕌❤️${data.number}: ${data.asma.ar.long} (${data.asma.en.long})❤️🕌
@@ -62,7 +62,7 @@ zokou({
       text: messageText,
       contextInfo: {
         externalAdReply: {
-          title: "NEXUS-AI QURAN SURAH",
+          title: "LUCVOICE-XMD QURAN SURAH",
           body: `We're reading: ${data.asma.en.long}`,
           mediaType: 1,
           thumbnailUrl: conf.URL, 
@@ -79,7 +79,7 @@ zokou({
         externalAdReply: {
           title: "Error Fetching Quran Passage",
           body: "Please try again later.",
-          thumbnailUrl: ".https://files.catbox.moe/pdhcob.jpeg", // Replace with a suitable thumbnail URL
+          thumbnailUrl: ".https://files.catbox.moe/t21l69.png", // Replace with a suitable thumbnail URL
           sourceUrl: conf.GURL,
           mediaType: 1,
           showAdAttribution: true,
