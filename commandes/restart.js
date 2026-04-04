c34946e47365ac11c939ac3456f79dc945e41617
Lukaa255 <lukabrand254@gmail.com>
2026-04-04 15:49:35 +0300

    Update restart.js

--- commandes/restart.js
+++ commandes/restart.js
@@ -1,31 +1,30 @@
-const {zokou}=require("../framework/zokou")
+const { zokou } = require("../framework/zokou");
 
+zokou({ nomCom: "restart", categorie: "Mods", reaction: "📴" }, async (dest, z, com) => {
 
+    const { repondre, superUser } = com;
 
+    if (!superUser) {
+        return repondre("❌ This command is for owner only");
+    }
 
+    const { exec } = require("child_process");
 
+    repondre("*♻️ Lucvoice-XMD bot is restarting...*");
 
+    exec("pm2 restart lucvoice-xmd", (error, stdout, stderr) => {
 
-zokou({nomCom:"restart",categorie:"Mods",reaction:"📴"},async(dest,z,com)=>{
+        if (error) {
+            console.log(error);
+            return;
+        }
 
+        if (stderr) {
+            console.log(stderr);
+            return;
+        }
 
-  
-const{repondre,ms,dev,superUser}=com;
+        console.log("Restart success:", stdout);
+    });
 
-  if(!superUser)
-  {
-    return repondre("This command is for owner only");
-  }
-
-  const {exec}=require("child_process")
-
-    repondre("*chuga-xmd bot is Restarting...*");
-
-  exec("pm2 restart all");
-  
-
-  
-
-
-
-})
+});
